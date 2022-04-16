cd71f01c80dcf3a9e70d8d464c8a2c8a90a01bba
Hassaan Nadeem <hassaan.butt9999@gmail.com>
2022-04-16 12:44:22 +0500

    production build

---
+++ build/precache-manifest.043835ff9a292e6229aa8b3691e7f48a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f71a4131da80c131bde8ddb361ec3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "993d7f712c3485bfb3a5",
+    "url": "/static/css/2.fd0507f5.chunk.css"
+  },
+  {
+    "revision": "4fe5398b6e0396e63d6a",
+    "url": "/static/css/main.8788188c.chunk.css"
+  },
+  {
+    "revision": "993d7f712c3485bfb3a5",
+    "url": "/static/js/2.741c54b7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.741c54b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe5398b6e0396e63d6a",
+    "url": "/static/js/main.5758180a.chunk.js"
+  },
+  {
+    "revision": "0c57f973157196477324",
+    "url": "/static/js/runtime-main.6b5ef6cf.js"
+  },
+  {
+    "revision": "696c44dd2f165a902c4f37b49eb41d1f",
+    "url": "/static/media/me2.696c44dd.jpg"
+  }
+]);
